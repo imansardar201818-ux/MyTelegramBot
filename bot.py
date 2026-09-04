4f93dac96e019a34f5e4502c0cf1de353f8b5c19
imansardar201818-ux <iman.sardar201818@gmail.com>
2026-09-04 21:10:57 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 import telebot
 
-TOKEN = "8904951204:AAF1UaBAzfD_OIrdqHiv8egIfs9a8g3ld4E"  7234567890:AAHdqTcvCH1vGWJxfSeofSAs0K5PALDsawE
+# فقط یک توکن رو نگه دار (همون جدیدی که از بات‌فادر گرفتی)
+TOKEN = "7234567890:AAHdqTcvCH1vGWJxfSeofSAs0K5PALDsawE"
 bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(commands=['start'])
@@ -8,4 +9,4 @@ def start(message):
     bot.reply_to(message, "🚀 سلام! ربات با موفقیت راه‌اندازی شد!")
 
 print("✅ ربات در حال اجراست...")
-bot.polling()
+bot.polling()  # اینجا bot هست، نه bot_infinity
